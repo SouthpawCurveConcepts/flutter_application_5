fecd410e78a9e3e0985b40a49f713f187857b71d
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-01-15 20:43:01 -0600

    2. text

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'I dare you to push the button a million times:',
             ),
             Text(
               '$_counter',
